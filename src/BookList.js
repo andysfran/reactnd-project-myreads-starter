d2b7405d67524da83f01dce9583f8d05f1bb58f9
Anderson Francisco <andao_11@yahoo.com>
2018-10-16 23:00:56 -0300

    Change books of the shelf
    Now you can move the books to another shelf

--- src/BookList.js
+++ src/BookList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export default class BookList extends Component {
+
+  goToSearch = () => {
+    const { history } = this.props;
+    history.push('/search');
+  }
+
   render() {
     return (
       <div className="list-books">
@@ -9,8 +15,11 @@ export default class BookList extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            
+            {this.props.children && this.props.children}
           </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.props.addBook() }>Add a book</a>
         </div>
       </div>
     );
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 import Book from './Book'
+import { update } from '../BooksAPI'
 
 export default class Bookshelf extends Component {
+
+  onUpdate = (book, status) => {
+    update({id: book}, status)
+      .then(() => {
+        this.props.onUpdateList();
+      });
+  }
 
   renderBooks = () => {
     const { bookList } = this.props;
     if (Array.isArray(bookList) && bookList.length > 0) {
       return bookList.filter((book) => book.shelf === this.props.filter)
-        .map((book) => <Book key={book.title} {...book} />)
+        .map((book) => {
+          return (
+            <Book
+              key={book.title}
+              onChangeBookStatus={(book, status) => this.onUpdate(book, status)}
+              {...book} 
+            />
+          );
+        });
     }
 
     return null;
